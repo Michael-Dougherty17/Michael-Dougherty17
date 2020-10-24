3f93b15c2498c7514dd1e0878bcbed4889587a2d
Michael-Dougherty17 <mdough28@terpmail.umd.edu>
2020-10-24 09:44:40 -0400

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,4 +1,8 @@
-// You may wish to find an effective randomizer function on MDN.
+function getRandomIntInclusive(min, max) {
+  const mn = Math.ceil(min);
+  const mx = Math.floor(max);
+  return Math.floor(Math.random() * (mx - mn + 1) + mn);
+}
 
 function range(int) {
   const arr = [];
@@ -8,7 +12,7 @@ function range(int) {
   return arr;
 }
 
-function sortFunction(a, b, key) {
+function sortByKey(org, compare, key) {
   if (a[key] < b[key]) {
     return -1;
   } if (a[key] > b[key]) {
@@ -29,7 +33,27 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
+      if (document.querySelector('.flex-inner')) {
+        document.querySelector('.flex-inner').remove();
+      }
+      document.createElement('.flex-inner');
+      const newArr = range(10);
+      const newArr2 = newArr.map(() => {
+        const number = getRandomIntInclusive(0, 243);
+        return fromServer[number];
+      });
+
+      const reverseList = newArr2.sort((org, compare) => sortByKey(org, compare, 'name'));
+      const ul = document.createElement('ol');
+      ul.className = '.flex-inner';
+      $('form').prepend(ul);
+      reverseList.forEach((el, i) => {
+        const li = document.createElement('li');
+        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
+        $(li).append(`<label for=${el.code}>${el.name} </label>`);
+        $(ul).append(li);
+      })
+
       console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
