7c9ba0aa5c05cff3616ee5db9ba2c61c463a50c4
Michael-Dougherty17 <mdough28@terpmail.umd.edu>
2020-10-27 17:35:16 -0400

    lab 7 wrok

--- server.js
+++ server.js
@@ -22,14 +22,15 @@ app.use((req, res, next) => {
 });
 
 app.route('/api')
-  .get((req, res) => {
+  .get(async (req, res) => {
     console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
+    console.log('Fetch request data', json);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
-    res.json(countries);
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    res.json(json);
   });
 
 app.listen(port, () => {
