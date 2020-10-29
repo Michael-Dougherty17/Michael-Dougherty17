e65c460d1f790aad149eb11d3bf6d09240eecc72
Michael-Dougherty17 <mdough28@terpmail.umd.edu>
2020-10-29 19:50:21 -0400

    Update script.js

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,63 +1,19 @@
-const chart = new CanvasJS.Chart('chartContainer', {
-  animationEnabled: true,
-
-  title: {
-    text: 'Fortune 500 Companies by Country',
-  },
-  axisX: {
-    interval: 1,
-  },
-  axisY2: {
-    interlacedColor: 'rgba(1,77,101,.2)',
-    gridColor: 'rgba(1,77,101,.1)',
-    title: 'Number of Companies',
-  },
-  data: [
-    {
-      type: 'bar',
-      name: 'companies',
-      axisYType: 'secondary',
-      color: '#014D65',
-      dataPoints: [
-        { y: 3, label: 'Sweden' },
-        { y: 7, label: 'Taiwan' },
-        { y: 5, label: 'Russia' },
-        { y: 9, label: 'Spain' },
-        { y: 7, label: 'Brazil' },
-        { y: 7, label: 'India' },
-        { y: 9, label: 'Italy' },
-        { y: 8, label: 'Australia' },
-        { y: 11, label: 'Canada' },
-        { y: 15, label: 'South Korea' },
-        { y: 12, label: 'Netherlands' },
-        { y: 15, label: 'Switzerland' },
-        { y: 25, label: 'Britain' },
-        { y: 28, label: 'Germany' },
-        { y: 29, label: 'France' },
-        { y: 52, label: 'Japan' },
-        { y: 103, label: 'China' },
-        { y: 134, label: 'US' },
-      ],
-    },
-  ],
-});
-chart.render();
-function convertRestaurantsToCategories(restaurantList) {
-  // process your restaurants here!
-  return list;
-}
-
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
   CanvasJS.addColorSet('customColorSet1', [
-    // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+    '#deeaee',
+    '#b1cbbb',
+    '#eea29a',
+    '#c94c4c'
   ]);
 
   return {
     animationEnabled: true,
     colorSet: 'customColorSet1',
     title: {
-      text: 'Change This Title'
+      text: 'Places to Eat Out In Future'
     },
     axisX: {
       interval: 1,
@@ -66,16 +22,39 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     axisY2: {
       interlacedColor: 'rgba(1,77,101,.2)',
       gridColor: 'rgba(1,77,101,.1)',
-      title: 'Change This Title',
+      title: 'Restaurant Type',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {
+        customBreaks: [
+          {
+            startValue: 40,
+            endValue: 50,
+            color: 'orange',
+            type: 'zigzag'
+          },
+          {
+            startValue: 85,
+            endValue: 100,
+            color: 'orange',
+            type: 'zigzagged'
+          },
+          {
+            startValue: 140,
+            endValue: 175,
+            color: 'orange',
+            type: 'zigzagged'
+          }
+        ]
+      }
     },
-    data: [{
-      type: 'bar',
-      name: 'restaurants',
-      axisYType: 'secondary',
-      dataPoints: datapointsFromRestaurantsList
-    }]
+    data: [
+      {
+        type: 'bar',
+        name: 'restaurants',
+        axisYType: 'secondary',
+        dataPoints: datapointsFromRestaurantsList
+      }
+    ]
   };
 }
 
